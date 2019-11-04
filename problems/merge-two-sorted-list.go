e65e54f77b042026e41cc1351f49c6623f646c0e
Alicia HAN <alicia.han007@gmail.com>
2019-11-04 07:39:42 +0100

    :floppy_disk: save works

---
+++ problems/merge-two-sorted-list.go
@@ -0,0 +1,21 @@
+package problems
+
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
